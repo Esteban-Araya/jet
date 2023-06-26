fef2c3fd22fe08b5ef1d00342ffa1ecb64246423
Esteban <estebanulises2004@gmail.com>
2023-06-26 17:35:26 -0300

    record

--- app/models.py
+++ app/models.py
@@ -44,8 +44,16 @@ class Users(AbstractBaseUser):
 
 
 class Devices(models.Model):
-    id = models.CharField(max_length=30, primary_key=True, null=False,unique=True, editable=False)
+    id = models.CharField(max_length=30, primary_key=True, null=False,unique=True)
     name = models.CharField(max_length=40, null=False)
     id_user_main = models.ForeignKey(Users,on_delete=models.CASCADE,  related_name="my_devices" )
     device_type = models.CharField(max_length=50, null=False )
     users_id = models.ManyToManyField("Users", related_name="devices", null=True)
+    turn_on = models.BooleanField(null=False, default=False)
+
+class Record(models.Model):
+    user_id = models.ForeignKey(Users,on_delete=models.CASCADE,  related_name="record" )
+    device_id = models.ForeignKey(Devices,on_delete=models.CASCADE,  related_name="record")
+    turn_on = models.BooleanField(null=False, default=False)
+    time = models.DateTimeField(auto_now_add=True)
+
